48ff0a3d138219f02744c42d12655895e28fd143
Byron <byronochieng01@gmail.com>
2024-04-25 12:00:56 +0300

    added getters n setters

--- src/main/java/com/rerec/fleetdb/entities/WorkInstructionItems.java
+++ src/main/java/com/rerec/fleetdb/entities/WorkInstructionItems.java
@@ -3,7 +3,7 @@ package com.rerec.fleetdb.entities;
 import jakarta.persistence.*;
 
 @Entity
-public class ProposedWork {
+public class WorkInstructionItems {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +16,28 @@ public class ProposedWork {
     @ManyToOne
     @JoinColumn(name="workInstruction_id")
     private WorkInstruction workInstruction;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Checklist getChecklist() {
+        return checklist;
+    }
+
+    public void setChecklist(Checklist checklist) {
+        this.checklist = checklist;
+    }
+
+    public WorkInstruction getWorkInstruction() {
+        return workInstruction;
+    }
+
+    public void setWorkInstruction(WorkInstruction workInstruction) {
+        this.workInstruction = workInstruction;
+    }
 }
