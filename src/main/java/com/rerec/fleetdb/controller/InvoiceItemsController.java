25d4c5a5b084fc91be5ab5e74db0726822fde373
Byron <byronochieng01@gmail.com>
2024-04-25 11:46:36 +0300

    InvoiceItemsController and its html file

---
+++ src/main/java/com/rerec/fleetdb/controller/InvoiceItemsController.java
@@ -0,0 +1,50 @@
+package com.rerec.fleetdb.controller;
+
+import com.rerec.fleetdb.entities.Invoice;
+import com.rerec.fleetdb.entities.InvoiceItems;
+import com.rerec.fleetdb.entities.Vehicle;
+import com.rerec.fleetdb.service.InvoiceItemsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class InvoiceItemsController {
+
+    private final InvoiceItemsService invoiceItemsService;
+
+    public InvoiceItemsController(InvoiceItemsService invoiceItemsService){
+        this.invoiceItemsService = invoiceItemsService;
+    }
+
+    @GetMapping("/alterInvoiceItems/{no}")
+    public String addInvoiceItems(@PathVariable (value = "no") Long no, Model model){
+        InvoiceItems invoiceItems = new InvoiceItems();
+
+        model.addAttribute("invoiceItem", invoiceItems);
+        model.addAttribute("listOfChecklist", invoiceItemsService.getAllChecklist());
+        model.addAttribute("invoiceFromDB", invoiceItemsService.getInvoiceByID(no));
+
+        return "alterInvoiceItems";
+    }
+
+    @PostMapping("/saveInvoiceItems")
+    public String saveInvoiceItem(@ModelAttribute("invoiceItem") InvoiceItems invoiceItems){
+        invoiceItemsService.saveInvoiceItem(invoiceItems);
+        return "redirect:/invoices";
+    }
+
+    /*
+    @GetMapping("/Vehicle/{numberPlate}")
+    public String updateExistingVehicle(@PathVariable(value = "numberPlate") String numberPlate, Model model){
+        Vehicle vehicle = vehicleService.getVehicleByNumberPlate(numberPlate);
+
+        // set vehicle as a model attribute to pre-populate the form
+        model.addAttribute("vehicle", vehicle);
+
+        return "updateVehicle";
+    }*/
+}
